cb2f9cc827b58e5d68084111a4e758917ac6db30
ZiToonn <durand.olivier.34@gmail.com>
2022-03-07 07:34:07 +0100

    end add ggle analytic, wait 1day /2

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,16 @@ export default {
     '@/assets/css/default.css'
   ],
 
+  // Google Analytics
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS_ID,
+  },
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: process.env.GOOGLE_ANALYTICS_ID
+    }
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: './plugins/vue-carousel.js', mode: 'client'},
   { src: './plugins/vue-country-flag.js' }],
@@ -47,6 +57,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/google-analytics'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
