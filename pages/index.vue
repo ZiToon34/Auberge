7ffdfecfae7db6154059387e8d20cac34346e223
ZiToonn <durand.olivier.34@gmail.com>
2022-06-27 20:57:28 +0200

    background cover

--- pages/index.vue
+++ pages/index.vue
@@ -28,6 +28,7 @@ export default {
     text-align: center;
     background: url("~/assets/img/navacelles1.jpg") center center / cover no-repeat;
     background-position: center;
+    background-size: cover;
     justify-content: center;
     width: 100%;
     height: 660px;
