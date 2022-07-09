329f5d0b527f3c1cbe7980ee95646a593288b332
ZiToon34 <77312729+ZiToon34@users.noreply.github.com>
2022-07-09 12:17:29 +0200

    update mail link

--- pages/reservation.vue
+++ pages/reservation.vue
@@ -144,7 +144,7 @@
       </div>
       <div>
         <br>
-    <a href="mailto:durand.olivier.34@gmail.com?subject=Reservation&body=Demande de Reservation" style="display: flex; flex-direction: column; justify-content: center; align-items: center; color: red"><strong>En cas de Problème lors de l'envoi du formulaire, cliquez ici</strong></a>
+    <a href="mailto:aubergedelacascade@orange.fr?subject=Reservation&body=Demande de Reservation" style="display: flex; flex-direction: column; justify-content: center; align-items: center; color: red"><strong>En cas de Problème lors de l'envoi du formulaire, cliquez ici</strong></a>
   </div>
     </div>
     
