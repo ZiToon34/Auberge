c213d1f9f73db901d2f7132f90346e557ef4ea64
ZiToonn <durand.olivier.34@gmail.com>
2022-11-02 18:46:06 +0100

    update popup

--- components/header.vue
+++ components/header.vue
@@ -60,12 +60,12 @@
       <div v-if="showMenu" @click="showMenu = false" id="croix">
         <img src="@/assets/img/croix.png" alt="boutton croix" />
       </div>
-      <div id="popup">
+      <popup id="popup">
         <div style="text-align: center; font-size: 42px !important;
     line-height: 42px !important; margin-bottom: 5px; margin-top: 20px;"> Fermeture Annuelle </div>
         <div style="text-align: center; margin-top: 15px; font-size: 20px; line-height: 30px; font-family: 'Playfair Display', Arial, Tahoma, sans-serif; color: #af8842;"> L'Auberge de la Cascade est fermée pour la saison 2022, elle sera heureuse de vous retrouver en Avril 2023 pour sa réouverture.</div>
         <div style="text-align: center; margin-top: 5px; font-size: 18px !important; line-height: 42px !important; margin-bottom: 5px;"> A très bientôt !</div>
-      </div>
+      </popup>
     </header>
   </div>
 </template>
@@ -100,20 +100,19 @@ export default {
 
 <style scoped>
 
-#popup {
+#popup{
     border-style: solid;
     border-color: #af8842;
     background-color: white;
     position: fixed;
     box-shadow: rgba(0, 0, 0, 1) 0px 54px 55px, rgba(0, 0, 0, 1) 0px -10px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-    width: 600px;
-    height: 200px;
+    width: 100%;
+    height: 25vh;
     top: 308px;
-    left: 33%;
     opacity: 1;
     display: block;
     z-index: 1999999999;
-}
+  }
 
 #selectLang {
   position: relative;
@@ -234,7 +233,42 @@ export default {
   display: none;
 }
 
+@media screen and (max-width: 410px){
+
+#popup{
+    border-style: solid;
+    border-color: #af8842;
+    background-color: white;
+    position: fixed;
+    box-shadow: rgba(0, 0, 0, 1) 0px 54px 55px, rgba(0, 0, 0, 1) 0px -10px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    width: 100%;
+    height: 35vh;
+    top: 308px;
+    opacity: 1;
+    display: block;
+    z-index: 1999999999;
+  }
+
+
+}
+
 @media screen and (min-width: 1285px) {
+
+  #popup {
+    border-style: solid;
+    border-color: #af8842;
+    background-color: white;
+    position: fixed;
+    box-shadow: rgba(0, 0, 0, 1) 0px 54px 55px, rgba(0, 0, 0, 1) 0px -10px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    width: 650px;
+    height: 200px;
+    top: 308px;
+    left: 25vw;
+    opacity: 1;
+    display: block;
+    z-index: 1999999999;
+}
+
   #listLang {
   padding: 5px;
   background-color: rgba(0, 0, 0, 0.3);
