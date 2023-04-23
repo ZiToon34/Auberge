833e2a55812dc218ad3e39187ffe1b99b47c6753
ZiToonn <durand.olivier.34@gmail.com>
2023-04-24 00:27:29 +0200

    windows location in option

--- pages/reservation.vue
+++ pages/reservation.vue
@@ -191,7 +191,7 @@ export default {
             swal("Problème d'envoi, veuillez réesayer", error.text);
           }
         )
-      window.location.href = "https://www.auberge-de-la-cascade.fr/formsend"
+      //window.location.href = "https://www.auberge-de-la-cascade.fr/formsend"//
     },
   },
   head: {
