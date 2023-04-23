40c286b301d1f3b55e8d04ab692c207f8e2120db
ZiToonn <durand.olivier.34@gmail.com>
2023-04-23 23:44:45 +0200

    reloadpage form when send

--- pages/reservation.vue
+++ pages/reservation.vue
@@ -13,7 +13,7 @@
     <p style="display: flex; flex-direction: column; justify-content: center; align-items: center; color: black"><strong>Ceci est un formulaire de PRÉ-RÉSERVATION (Réponse sous 48H) </strong></p>
   </div>
       <div class="box1" style="display: block">
-        <form id="myForm" ref="form" @submit.prevent="sendEmail">
+        <form id="myForm" ref="form" @submit.prevent="sendEmail, submitForm" >
           <label for="form_name">{{$t('nameandsur')}}</label>
           <input
             id="form_name"
@@ -164,6 +164,10 @@ export default {
     };
   },
   methods: {
+     submitForm() {
+      // Code pour traiter les données du formulaire
+      location.reload(); // Actualiser la page
+    },
     async sendEmail() {
       try {
         const token = await this.$recaptcha.getResponse();
