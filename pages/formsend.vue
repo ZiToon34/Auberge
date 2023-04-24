b37fd1103d2ebec4c097d75653566b89f7b12936
ZiToon34 <77312729+ZiToon34@users.noreply.github.com>
2023-04-24 11:47:38 +0200

    change confirmation

--- pages/formsend.vue
+++ pages/formsend.vue
@@ -3,8 +3,8 @@
     
   <div id="container-nava"><img class="nava" alt="photo Navacelles" src="@/assets/img/navacelles1.jpg"></div>
   <div class="text" style="opacity: 1; box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;">
-    <h2 class="nom">Votre e-mail a bien été envoyé.</h2>
-    <h3 class="description">Nous traiterons votre demande sous 48 heures.</h3>
+    <h2 class="nom">Votre demande de pré-réservation a bien été envoyé.</h2>
+    <h3 class="description">Elle sera traitée sous 48 heures.</h3>
     <p class="info"><strong>En vous remerciant.</strong></p>
   </div></div>
 
