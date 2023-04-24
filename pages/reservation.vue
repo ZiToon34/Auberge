2c46c2d8041aad200764e7cd031c07307a49f8a7
ZiToon34 <77312729+ZiToon34@users.noreply.github.com>
2023-04-24 12:25:14 +0200

    Update reservation.vue

--- pages/reservation.vue
+++ pages/reservation.vue
@@ -139,7 +139,7 @@
 
           <recaptcha />
           <br />
-          <input type="submit" id="submitBtn" value="Envoyer" ref="submitButton"/>
+          <button type="submit" id="submitBtn" value="Envoyer" ref="submitButton"/>
         </form>
       </div>
       <div>
@@ -164,6 +164,14 @@ export default {
     };
   },
   methods: {
+  {
+    disableSubmitButton() {
+      this.$refs.submitButton.disabled = true;
+    }
+  },
+  mounted() {
+    this.$refs.submitButton.addEventListener('click', this.disableSubmitButton);
+  },
     async sendEmail() {
       try {
         const token = await this.$recaptcha.getResponse();
