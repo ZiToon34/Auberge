43f82eadbd9142802fced48e3892e741fc54aaa5
ZiToon34 <77312729+ZiToon34@users.noreply.github.com>
2023-04-24 13:06:41 +0200

    Update reservation.vue

--- pages/reservation.vue
+++ pages/reservation.vue
@@ -139,7 +139,7 @@
 
           <recaptcha />
           <br />
-          <a type="submit" id="submitBtn" value="Envoyer"><a/>
+          <a type="submit" id="submitBtn" value="Envoyer" href="https://www.auberge-de-la-cascade.fr/formsend"><a/>
         </form>
       </div>
       <div>
