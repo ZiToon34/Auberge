2e52fe476df3479190cc1cf552f856a4423b54c7
ZiToon34 <77312729+ZiToon34@users.noreply.github.com>
2023-04-26 14:06:46 +0200

    Update header.vue

--- components/header.vue
+++ components/header.vue
@@ -3,30 +3,30 @@
     <header class="header">
       <nav :class="{ hide_smallscreen: !showMenu, topbar: true }">
         <div class="nav_topbar" @click="showMenu = false">
-          <NuxtLink :to="localePath('/')" id="acceuil" ontouchstart="">
+          <NuxtLink :to="localePath('/')" id="acceuil">
             <img src="@/assets/img/acceuil.png" alt="bouton acceuil" />
           </NuxtLink>
         </div>
         <div class="nav_topbar" @click="showMenu = false">
-          <NuxtLink :to="localePath('/hotel/')" ontouchstart=""> {{ $t("hotel") }} </NuxtLink>
+          <NuxtLink :to="localePath('/hotel/')"> {{ $t("hotel") }} </NuxtLink>
         </div>
         <div class="nav_topbar" @click="showMenu = false">
-          <NuxtLink :to="localePath('/restaurant/')" ontouchstart="">
+          <NuxtLink :to="localePath('/restaurant/')">
             {{ $t("restaurant") }}
           </NuxtLink>
         </div>
         <div class="nav_topbar" @click="showMenu = false">
-          <NuxtLink :to="localePath('/alentour/')" ontouchstart="">
+          <NuxtLink :to="localePath('/alentour/')">
             {{ $t("navaAndAround") }}
           </NuxtLink>
         </div>
         <div class="nav_topbar" @click="showMenu = false">
-          <NuxtLink :to="localePath('/contact/')" ontouchstart="">
+          <NuxtLink :to="localePath('/contact/')">
             {{ $t("contact") }}
           </NuxtLink>
         </div>
         <div class="nav_topbar" @click="showMenu = false">
-          <NuxtLink :to="localePath('/reservation/')" id="reservation" ontouchstart="">
+          <NuxtLink :to="localePath('/reservation/')" id="reservation">
             {{ $t("reservation") }}
           </NuxtLink>
         </div>
