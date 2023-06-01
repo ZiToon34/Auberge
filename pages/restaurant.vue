79286f174cbd9b8455d040b9ca98c6797f53631b
ZiToon34 <77312729+ZiToon34@users.noreply.github.com>
2023-06-02 00:40:51 +0200

    Update restaurant.vue

--- pages/restaurant.vue
+++ pages/restaurant.vue
@@ -128,7 +128,7 @@ export default {
         scrollPerPage: true,
         paginationEnabled: true,
         autoplay: true,
-        autoplayTimeout: 1300
+        autoplayTimeout: 1500
       }
     }
   },
