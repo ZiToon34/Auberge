25b3143c741470ef55524b23d1a944f8ca66914d
Zitoon34 <durand.olivier.34@gmail.com>
2025-04-12 21:33:03 +0200

    change in pre resa

--- pages/reservation.vue
+++ pages/reservation.vue
@@ -8,8 +8,8 @@
       />
     </div>
     <div class="conteneur">
+      <a class="reservation-btn" href="https://larzac34.netlify.app/reservation">🍽️ Réserver au Restaurant en ligne</a>
 <div>
-<a class="reservation-btn" href="https://larzac34.netlify.app/reservation">🍽️ Réserver au Restaurant en ligne</a>
       <p style="display: flex; flex-direction: column; justify-content: center; align-items: center; color: red"> <strong>!! L'auberge se situe au hameau de Navacelles dans le cirque de Navacelles (non-pas à St Maurice-Navacelles mais sur sa commune) !!</strong></p>
     </div>
     <div>
