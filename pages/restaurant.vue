255f6f0b3cccfb695a38b19c50f253160b2d0004
Zitoon34 <durand.olivier.34@gmail.com>
2025-04-12 20:10:19 +0200

    remove reservation

--- pages/restaurant.vue
+++ pages/restaurant.vue
@@ -12,7 +12,7 @@
     
     <h3 class="description">{{$t('descriptionrestaurant')}}</h3>
     <p class="info"><u><strong> {{$t('inforestaurant')}} </strong></u> <br>{{$t('ps')}}</p>
-  <a class="nom" href="https://larzac34.netlify.app/reservation" style="border-radius: 30px; background-color: black; color: aliceblue;">Reserver au Restaurant en ligne, cliquez ici</a>
+  <!--<a class="nom" href="https://larzac34.netlify.app/reservation" style="border-radius: 30px; background-color: black; color: aliceblue;">Reserver au Restaurant en ligne, cliquez ici</a> -->
   </div>
 
 
