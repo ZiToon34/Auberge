637525dfa814aabc7e19d19ccabbe4dbbc20f6d7
ZiToon34 <77312729+ZiToon34@users.noreply.github.com>
2026-02-23 13:21:13 +0100

    Update restaurant reservation link

--- pages/reservation.vue
+++ pages/reservation.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="conteneur">
       <div class="button-container">
-  <a class="reservation-btn" href="https://www.auberge-de-la-cascade.bookatable.fr">🍽️ Réserver au Restaurant en ligne, <span class="click-here">Cliquez ICI</span></a>
+  <a class="reservation-btn" href="https://auberge-de-la-cascade.bookatable.fr">🍽️ Réserver au Restaurant en ligne, <span class="click-here">Cliquez ICI</span></a>
         </div>
         <div class="button-container">
   <a class="reservation-btn" href="https://auberge-de-la-cascade.fr/reservationHotel">🏨 Réserver pour l'Hébergement, <span class="click-here">Cliquez ICI</span></a>
