362e9040ef2e3f3396cd6341daf4efa1d6264f17
ZiToon34 <77312729+ZiToon34@users.noreply.github.com>
2026-04-04 21:59:42 +0200

    Remove menu table from restaurant.vue
    Removed menu items and their prices from the restaurant page.

--- pages/restaurant.vue
+++ pages/restaurant.vue
@@ -30,19 +30,6 @@
   <div class="menu">
 
     <div class="plat" style="box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;">
-      <div class="listeMenu"> <strong>{{$t('listemenu')}}</strong></div>
-      <table>
-        <tbody>
-        <tr>
-          <td class="listePlat">{{$t('listeplat1')}}</td>
-          <td class="prixPlat">{{$t('prixEP')}}</td>
-        </tr>
-        <tr>
-          <td class="listePlat">{{$t('listeplat2')}}</td>
-          <td class="prixPlat">{{$t('prixEPD')}}</td>
-        </tr>
-        </tbody>
-      </table>
       <div class="listeMenu"> <strong>{{$t('ardoise')}}</strong></div>
       <table>
         <tbody>
